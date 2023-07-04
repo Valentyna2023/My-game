c3b002db1d3728f3e29fe07fd60d7b28121789ce
Valentyna2023 <125480372+Valentyna2023@users.noreply.github.com>
2023-07-04 16:14:28 -0700

    first commit

---
+++ src/assets/js/languages.js
@@ -0,0 +1,18 @@
+export const languages = {
+  en: {
+    editions: "Editions",
+    controller: "Controller",
+    about: "About",
+    explore: "Explore",
+    news: "News",
+    faq: "FAQ",
+  },
+  uk: {
+    editions: "gdhh",
+    controller: "dsfdf",
+    about: "sdfsd",
+    explore: "fdsg",
+    news: "sdgsd",
+    faq: "fsfsь",
+  },
+};
